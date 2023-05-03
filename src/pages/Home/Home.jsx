1264bec2d5f5a3aa0a7733fb43a923e80230083b
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-05-03 11:57:46 +0500

    feat: add home page links

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
       <div className="home">
-        
+        <div className="home__links">
+          <Link to="login">Вход</Link>
+          <Link to="form">Регистрация</Link>
+          <Link to="competitions">Соревнования</Link>
+        </div>
       </div>
     </>
   );
