e155cf6116c7f1e9a1a831a150c6fa08408d2ddb
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-05-04 18:38:16 +0500

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Competitions from "./components/competitions/Competitions";
 import Choice from "./components/choice/Choice";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<NavBar/>}>
+        <Route path="/" element={[<NavBar/>,<Footer />]}>
           <Route index element={<Home/>}/>
           <Route path="description" element={<Description/>}/>
           <Route path="competitions" element={<Competitions/>}/>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            Footer
+        </footer>
+    );
+}
+
+export default Footer;
